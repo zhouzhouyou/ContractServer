ef90614addce7fae75fca304f300fcc2cd847a25
周洲游 <1445352275@qq.com>
2019-12-06 11:48:34 +0800

    完善了javadoc

--- src/main/java/yuri/contract/server/controlller/UserController.java
+++ src/main/java/yuri/contract/server/controlller/UserController.java
@@ -1,6 +1,7 @@
 package yuri.contract.server.controlller;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ import java.util.List;
 @Api(tags = "To Control user operations")
 @RestController
 @RequestMapping("/api")
-public class UserController extends BaseController{
+public class UserController extends BaseController {
     private final SignInService loginService;
     private final UserService userService;
 
@@ -31,25 +32,46 @@ public class UserController extends BaseController{
         this.userService = userService;
     }
 
+    /**
+     * 处理用户登录
+     *
+     * @param user          用户的信息
+     * @param bindingResult 绑定结果
+     * @return 登录成功返回token
+     */
     @ApiOperation("用户登录，会返回token")
     @CrossOrigin
     @PostMapping(value = "/signIn")
     @ResponseBody
     public ResponseEntity<String> signIn(@RequestBody User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors())
+            return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
         return loginService.signIn(user.getName(), user.getPassword());
     }
 
+    /**
+     * 处理用户注册
+     *
+     * @param user          新用户信息
+     * @param bindingResult 绑定结果
+     * @return 注册成功返回token
+     */
     @ApiOperation("用户注册，会返回token")
     @CrossOrigin
     @PostMapping(value = "/signUp")
     @ResponseBody
     public ResponseEntity<String> signUp(@RequestBody User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors())
+            return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
 
         return loginService.signUp(user.getName(), user.getPassword());
     }
 
+    /**
+     * 测试，需要 {@link NeedToken#DRAFT_CONTRACT}
+     *
+     * @return 有起草合同的权限就会返回
+     */
     @ApiIgnore
     @CrossOrigin
     @RequestMapping("/test")
@@ -59,27 +81,47 @@ public class UserController extends BaseController{
         return ResponseFactory.success("test!");
     }
 
-
+    /**
+     * 删除一个用户,需要{@link NeedToken#DELETE_USER}
+     *
+     * @param name          用户名
+     * @param bindingResult 绑定结果
+     * @return 删除用户是否成功
+     */
     @ApiOperation("删除用户")
     @CrossOrigin
     @DeleteMapping(value = "/user/delete")
     @ResponseBody
     @NeedToken(function = NeedToken.DELETE_USER)
     public ResponseEntity<String> deleteUser(@RequestBody Name name, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors())
+            return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
         return userService.deleteUserByName(getOperator(), name.getName());
     }
 
+    /**
+     * 插入用户，需要{@link NeedToken#INSERT_USER}
+     *
+     * @param user          用户信息
+     * @param bindingResult 绑定结果
+     * @return 用户名
+     */
     @ApiOperation("插入用户（这个不是注册，是直接插入，所以不会返回token）")
     @CrossOrigin
     @PutMapping(value = "/user/insert")
     @ResponseBody
     @NeedToken(function = NeedToken.INSERT_USER)
     public ResponseEntity<String> insertUser(@RequestBody User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors())
+            return ResponseFactory.badRequest(bindingResult.getFieldError().getDefaultMessage());
         return userService.insertUser(getOperator(), user);
     }
 
+    /**
+     * 获取所有用户名，需要 {@link NeedToken#SELECT_USER}
+     *
+     * @return 所有的用户名
+     */
     @ApiOperation("获取所有用户名, List<String>")
     @CrossOrigin
     @GetMapping(value = "/user/selectAll")
